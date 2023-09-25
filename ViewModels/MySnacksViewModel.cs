8fac3274257742716ecc9805425fc7daf01b3625
HadrielWonda <olawalebash2017@gmail.com>
2023-09-25 19:16:37 +0100

    added community tooolkit

--- ViewModels/MySnacksViewModel.cs
+++ ViewModels/MySnacksViewModel.cs
@@ -12,4 +12,32 @@ public partial class MySnacksViewModel : ObservableObject
 {
     [ObservableProperty]
     List<Snack> snacks;
+
+    public MySnacksViewModel()
+    {
+        LoadSnacks();
+    }
+
+    private void LoadSnacks()
+    {
+        Snacks = new()
+        {
+            new Snack(
+                Name: "Wales Treat 1",
+                Description:"Wales first treat for you",
+                StartDateTime: DateTime.UtcNow.AddDays(1),
+                EndDateTime: DateTime.UtcNow.AddDays(1).AddHours(1),
+                Image: new Uri("https://imgbb.com/"),
+                Savoury: new List<string> {"Food1","Food2","Food3"},
+                Sweet:new List<string> {"Sweet1","Sweet2","Sweet3"}),
+             new Snack(
+                Name: "Wales Treat 2",
+                Description:"Wales next treat for you 😋😋😋",
+                StartDateTime: DateTime.UtcNow.AddDays(1),
+                EndDateTime: DateTime.UtcNow.AddDays(1).AddHours(1),
+                Image: new Uri("https://imgbb.com/"),
+                Savoury: new List<string> {"FoodA","FoodB","FoodC"},
+                Sweet:new List<string> {"SweetA","SweetB","SweetC"})
+        }
+    }
 }
