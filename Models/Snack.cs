679d77c6a1bb404de0d23e59166478aa922ebc96
HadrielWonda <olawalebash2017@gmail.com>
2023-09-25 18:56:48 +0100

    included community toolkit

---
+++ Models/Snack.cs
@@ -0,0 +1,10 @@
+﻿namespace SnackerUI.Models;
+
+public record Snack(
+    string Name,
+    string Description,
+    DateTime StartDateTime,
+    DateTime EndDateTime,
+    Uri Image,
+    List<string> Savoury,
+    List<string> Sweet);
---
+++ ViewModels/MySnacksViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SnackerUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackerUI.ViewModels;
+
+public partial class MySnacksViewModel : ObservableObject 
+{
+    [ObservableProperty]
+    List<Snack> snacks;
+}
